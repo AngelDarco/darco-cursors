a24004014a21328a8ff5e403eb1092963d119d86
Angel Darco <angel92-eme@hotmail.com>
2024-01-07 13:44:17 -0500

    :racehorse: main files to test the module created

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
 import "./App.css";
-
+import styles from "./App.module.css";
+import Cursors from "./src/components/Cursors";
 function App() {
-  return <h1>Working on it ...</h1>;
+  return (
+    <Cursors>
+      <h1 className={styles.title}>Cursors Working ...</h1>
+      <div className={styles.mainContainer}>
+        my container
+        <input type="text" placeholder="Enter text" name="" id="" />
+        <a href="">This is a link</a>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum,
+        similique maxime eius repellendus nulla enim suscipit facilis, qui
+        expedita ab, totam commodi quia. Accusamus explicabo quae sit eum
+        possimus magnam.
+        <a href="">this is the second link</a>
+      </div>
+    </Cursors>
+  );
 }
 
 export default App;
