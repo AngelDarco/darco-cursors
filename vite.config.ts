02e701ce2df8116ae1e13a52c0f8399e43122c4c
Angel Darco <angel92-eme@hotmail.com>
2024-01-14 08:01:19 -0500

    :wrench: config to build Cursors module

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,41 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+
+import dts from "vite-plugin-dts";
+import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  build: {
+    lib: {
+      name: "index",
+      fileName: "index",
+      entry: "src/components/Cursors.tsx",
+      formats: ["es", "umd", "cjs"],
+    },
+    minify: true,
+    emptyOutDir: true,
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        dir: "dist",
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+    assetsDir: "assets",
+  },
+  plugins: [
+    react(),
+    cssInjectedByJsPlugin(),
+    dts({
+      rollupTypes: true,
+      outDir: "dist/@types",
+      insertTypesEntry: true,
+      include: ["src/**/*.ts", "src/**/*.tsx"],
+    }),
+  ],
+  assetsInclude: ["**/*.cur"],
+});
