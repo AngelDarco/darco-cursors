ca0b42c34aedd6336f99d2ed4879e9d8e2eaf729
Angel Darco <angel92-eme@hotmail.com>
2024-01-17 09:26:18 -0500

    :sparkles: files to test module updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,23 @@
 import styles from "./App.module.css";
-import Cursors from "./components/Cursors";
+
+// import Cursors from "./components/index";
+
+import Cursors from "darco-cursors";
+
+// import Cursors from "../dist/";
+
 function App() {
   return (
-    <Cursors type="cyan">
-      <h1 className={styles.title}>Cursors Working ...</h1>
-      <div className={styles.mainContainer}>
-        my container
-        <input type="text" placeholder="Enter text" name="" id="" />
-        <a href="">This is a link</a>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum,
-        similique maxime eius repellendus nulla enim suscipit facilis, qui
-        expedita ab, totam commodi quia. Accusamus explicabo quae sit eum
-        possimus magnam.
-        <a href="">this is the second link</a>
-        <textarea name="" id="" cols={40} rows={10}></textarea>
-        <button>button</button>
-        <div
-          style={{
-            width: "500px",
-            height: "150px",
-            border: "1px solid red",
-            margin: "20px",
-          }}
-        ></div>
-      </div>
+    <Cursors type="blue" className={styles.mainContainer}>
+      <h1 className={styles.title}>My awesome cursors ...</h1>
+      <textarea
+        cols={30}
+        rows={10}
+        placeholder="This is a textarea with text cursor"
+      ></textarea>
+      <a href="#"> Link with pointer cursor</a>
+      <a href="#">Link with pointer cursor</a>
+      <a href="#">Link with pointer cursor</a>
     </Cursors>
   );
 }
