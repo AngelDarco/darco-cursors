730a2b15c7a28b66b2da2b9af8613cf68b760bbe
AngelDarco <angel92-eme@hotmail.com>
2024-01-19 14:49:34 -0500

    :fire: unnecessary files deleted after refactor

--- src/types/custom.d.ts
+++
@@ -1,9 +0,0 @@
-declare module "*.cur" {
-  const value: string;
-  export default value;
-}
-import CursorsStyles from "../utils/exportCursorStyles.ts";
-import { CustomCursorsProps } from "./index";
-
-declare const CursorsStyles: CustomCursorsProps;
-export { CursorsStyles };
