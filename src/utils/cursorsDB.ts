37ec02de1075a1255f474f2623ede3bbc11eaef0
AngelDarco <angel92-eme@hotmail.com>
2024-01-19 15:09:41 -0500

    :hammer: database with cursor paths

--- src/utils/cursorsDB.ts
+++ src/utils/cursorsDB.ts
@@ -1,9 +1,84 @@
+import generalBlue from "../assets/blue/NeonCursor04-Blue.cur";
+import pointerBlue from "../assets/blue/NeonLinkSelect04-Blue.cur";
+import textBlue from "../assets/blue/NeonTextSelect04-Blue.cur";
+
+import generalYellow from "../assets/yellow/NeonCursor07-Yellow.cur";
+import pointerYellow from "../assets/yellow/NeonLinkSelect07-Yellow.cur";
+import textYellow from "../assets/yellow/NeonTextSelect07-Yellow.cur";
+
+import generalRed from "../assets/red/NeonCursor01-Red.cur";
+import pointerRed from "../assets/red/NeonLinkSelect01-Red.cur";
+import textRed from "../assets/red/NeonTextSelect01-Red.cur";
+
+import generalPurple from "../assets/purple/NeonCursor03-Purple.cur";
+import pointerPurple from "../assets/purple/NeonLinkSelect03-Purple.cur";
+import textPurple from "../assets/purple/NeonTextSelect03-Purple.cur";
+
+import generalPink from "../assets/pink/NeonCursor02-Pink.cur";
+import pointerPink from "../assets/pink/NeonLinkSelect02-Pink.cur";
+import textPink from "../assets/pink/NeonTextSelect02-Pink.cur";
+
+import generalOrange from "../assets/orange/NeonCursor08-Orange.cur";
+import pointerOrange from "../assets/orange/NeonLinkSelect08-Orange.cur";
+import textOrange from "../assets/orange/NeonTextSelect08-Orange.cur";
+
+import generalGreen from "../assets/green/NeonCursor06-Green.cur";
+import pointerGreen from "../assets/green/NeonLinkSelect06-Green.cur";
+import textGreen from "../assets/green/NeonTextSelect06-Green.cur";
+
+import generalCyan from "../assets/cyan/NeonCursor05-Cyan.cur";
+import pointerCyan from "../assets/cyan/NeonLinkSelect05-Cyan.cur";
+import textCyan from "../assets/cyan/NeonTextSelect05-Cyan.cur";
+
 const blue = {
-  general: "./assets/blue/NeonCursor04-Blue.cur",
-  pointer: "./assets/blue/NeonLinkSelect04-Blue.cur",
-  text: "./assets/blue/NeonTextSelect04-Blue.cur",
+  general: generalBlue,
+  pointer: pointerBlue,
+  text: textBlue,
 };
 
+const yellow = {
+  general: generalYellow,
+  pointer: pointerYellow,
+  text: textYellow,
+};
+
+const red = {
+  general: generalRed,
+  pointer: pointerRed,
+  text: textRed,
+};
+
+const purple = {
+  general: generalPurple,
+  pointer: pointerPurple,
+  text: textPurple,
+};
+
+const pink = {
+  general: generalPink,
+  pointer: pointerPink,
+  text: textPink,
+};
+
+const orange = {
+  general: generalOrange,
+  pointer: pointerOrange,
+  text: textOrange,
+};
+
+const green = {
+  general: generalGreen,
+  pointer: pointerGreen,
+  text: textGreen,
+};
+
+const cyan = {
+  general: generalCyan,
+  pointer: pointerCyan,
+  text: textCyan,
+};
+
+/* 
 const yellow = {
   general: "./assets/yellow/NeonCursor07-Yellow.cur",
   pointer: "./assets/yellow/NeonLinkSelect07-Yellow.cur",
@@ -44,7 +119,7 @@ const cyan = {
   general: "./assets/cyan/NeonCursor05-Cyan.cur",
   pointer: "./assets/cyan/NeonLinkSelect05-Cyan.cur",
   text: "./assets/cyan/NeonTextSelect05-Cyan.cur",
-};
+}; */
 
 const defaultCursors = { blue, yellow, red, purple, pink, orange, green, cyan };
 
